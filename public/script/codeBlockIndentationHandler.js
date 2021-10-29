ad21f5f9fedb9019f4887be377791e77badfd2d1
BenocxX <mathis.cote@outlook.com>
2021-10-29 09:56:08 -0400

    Made the template more complete

--- public/script/codeBlockIndentationHandler.js
+++ public/script/codeBlockIndentationHandler.js
@@ -5,31 +5,33 @@
  */
 
 // get block however you want.
-let block = document.getElementById("the-code");
+let blocks = document.querySelectorAll(".the-code");
 
-// remove leading and trailing white space.
-let code = block.innerHTML
-    .split('\n')
-    .filter(l => l.trim().length > 0)
-    .join('\n');
-
-// find the first non-empty line and use its
-// leading whitespace as the amount that needs to be removed
-let firstNonEmptyLine = block.textContent
-    .split('\n')
-    .filter(l => l.trim().length > 0)[0];
-
-// using regex get the first capture group
-let leadingWhiteSpace = firstNonEmptyLine.match(/^([ ]*)/);
-
-// if the capture group exists, then use that to
-// replace all subsequent lines.
-if(leadingWhiteSpace && leadingWhiteSpace[0]) {
-    let whiteSpace = leadingWhiteSpace[0];
-    code = code.split('\n')
-        .map(l => l.replace(new RegExp('^' + whiteSpace + ''), ''))
+for (const block of blocks) {
+    // remove leading and trailing white space.
+    let code = block.innerHTML
+        .split('\n')
+        .filter(l => l.trim().length > 0)
         .join('\n');
-}
 
-// update the inner HTML with the edited code
-block.innerHTML = code;
+    // find the first non-empty line and use its
+    // leading whitespace as the amount that needs to be removed
+    let firstNonEmptyLine = block.textContent
+        .split('\n')
+        .filter(l => l.trim().length > 0)[0];
+
+    // using regex get the first capture group
+    let leadingWhiteSpace = firstNonEmptyLine.match(/^([ ]*)/);
+
+    // if the capture group exists, then use that to
+    // replace all subsequent lines.
+    if(leadingWhiteSpace && leadingWhiteSpace[0]) {
+        let whiteSpace = leadingWhiteSpace[0];
+        code = code.split('\n')
+            .map(l => l.replace(new RegExp('^' + whiteSpace + ''), ''))
+            .join('\n');
+    }
+
+    // update the inner HTML with the edited code
+    block.innerHTML = code;
+}
