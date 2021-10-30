7188e5e308bb3bf0fd1264acbb490490eb238fe9
BenocxX <mathis.cote@outlook.com>
2021-10-30 16:50:16 -0400

    Added 3 different modals

--- public/script/app.js
+++ public/script/app.js
@@ -1,13 +1,40 @@
 import { $on } from "./helpers.js"
 import * as md from "./modal.js"
 
-const modal = document.querySelector("#exampleModal")
-modal.addEventListener("modal.shown", function (e) {
+// Doc: Creating a basic modal
+const modalLaunchers = document.querySelectorAll("[data-modal-launcher]")
+for (const launcher of modalLaunchers) {
+    launcher.addEventListener("click", (e) => {
+        md.onModalLaunch(e)
+    })
+}
+
+// Doc: Customizing elements in the modal
+const modal2 = document.querySelector("#exampleModal2")
+modal2.addEventListener("modal.shown", function (e) {
     const dialog = this.querySelector(".modal-dialog")
     const dialogTitle = dialog.querySelector("h3")
     const dialogText = dialog.querySelector("p")
-    //const detail = e.detail
 
     dialogTitle.innerText = "My custom title"
     dialogText.innerText = "My custom text"
+})
+
+// Doc: Passing info through modal
+document.querySelector("#buttonExample3")
+    .addEventListener("click", (e) => {
+        md.onModalLaunch(e, {
+            title: "Where am I from?",
+            text: "I am text coming from the \"detail\" parameter!"
+        })
+    })
+
+const modal3 = document.querySelector("#exampleModal3")
+modal3.addEventListener("modal.shown", function (e) {
+    const dialog = this.querySelector(".modal-dialog")
+    const dialogTitle = dialog.querySelector("h3")
+    const dialogText = dialog.querySelector("p")
+
+    dialogTitle.innerText = e.detail.title
+    dialogText.innerText = e.detail.text
 })
