bdd5dfe58722f458976642273ed7f29dc385babb
BenocxX <mathis.cote@outlook.com>
2021-10-30 16:50:45 -0400

    Made the onModalLaunch() easier to use

--- public/script/modal.js
+++ public/script/modal.js
@@ -1,31 +1,20 @@
 import { $on } from "./helpers.js"
 
-$on("[data-modal-launcher]", "click", onModalLaunch)
-
-// TODO: Make modalType a value passed to the function by the user
-// TODO: Make detail an object passed to the function by the user
-function onModalLaunch(e) {
+export function onModalLaunch(e, detail) {
     const modalType = e.target.dataset.modalId
     const modal = document.querySelector("#" + modalType)
     const modalShownEvent = new CustomEvent("modal.shown", {
-        detail: {
-            test: "Test Value 123"
-        }
+        detail
     })
 
     modal.dispatchEvent(modalShownEvent)
-}
 
-const modal = document.querySelector(".modal")
-modal.addEventListener("modal.shown", function (e) {
-    const modal = this
     const shadow = document.querySelector(".modal-shadow")
-
-    modal.classList.add("show")
-    shadow.classList.add("show")
+    modal.classList.add("show");
+    shadow.classList.add("show");
 
     shadow.addEventListener("click", (e) => {
         modal.classList.remove("show");
         shadow.classList.remove("show");
     })
-})
+}
