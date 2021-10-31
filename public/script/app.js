0ac4590a7dc5cec88201dacf38f0e3d511235948
BenocxX <mathis.cote@outlook.com>
2021-10-31 18:12:38 -0400

    Use of a scrollSpy found on internet
    Source: https://codepen.io/zchee/pen/ogzvZZ

--- public/script/app.js
+++ public/script/app.js
@@ -1,6 +1,30 @@
 import { $on } from "./helpers.js"
 import * as md from "./modal.js"
 
+/***
+ * Source for custom plain js scrollspy: https://codepen.io/zchee/pen/ogzvZZ
+ */
+function scrollSpy() {
+    let sections = document.querySelectorAll(".doc-element");
+    let sectionCoordinates = {};
+    let i = 0;
+
+    Array.prototype.forEach.call(sections, function(e) {
+        sectionCoordinates[e.id] = e.offsetTop;
+    });
+
+    window.onscroll = function() {
+        let scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+        for (i in sectionCoordinates) {
+            if (sectionCoordinates[i] <= scrollPosition) {
+                document.querySelector('.active').setAttribute('class', ' ');
+                document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active');
+            }
+        }
+    };
+}
+scrollSpy()
+
 // Doc: Creating a basic modal
 const modalLaunchers = document.querySelectorAll("[data-modal-launcher]")
 for (const launcher of modalLaunchers) {
