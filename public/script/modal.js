623355a2c5a172b872e95b755279b68830ad1f7d
BenocxX <mathis.cote@outlook.com>
2021-11-10 20:01:09 -0500

    Close button working on alert modal

--- public/script/modal.js
+++ public/script/modal.js
@@ -4,13 +4,13 @@ export function showAlertButton() {
             <div class="modal-dialog">
                 <div class="modal-header-container">
                     <h3 class="modal-title">Alert !</h3>
-                    <button class="modal-button">X</button>
+                    <button data-close-button class="modal-button">X</button>
                 </div>
                 <div class="modal-content-container">
                     <p class="modal-text">This is an alert</p>
                 </div>
                 <div class="modal-footer-container">
-                    <button class="modal-button">Close</button>
+                    <button data-close-button class="modal-button">Close</button>
                 </div>
             </div>
         </div>
@@ -20,32 +20,45 @@ export function showAlertButton() {
     const alertModalContainer = document.querySelector(".alert-modal-container")
     alertModalContainer.innerHTML = html
 
-    const modal = document.querySelector("#alertModal")
-    const shadow = document.querySelector(".modal-shadow")
-    modal.classList.add("show");
-    shadow.classList.add("show");
+    const modal = document.querySelector("#alertModal");
+    const shadow = document.querySelector(".modal-shadow");
+    showModal(modal, shadow)
 
     shadow.addEventListener("click", (e) => {
-        modal.classList.remove("show");
-        shadow.classList.remove("show");
+        closeModal(modal, shadow);
     })
+
+    const closeButtons = modal.querySelectorAll("[data-close-button]");
+    for (const closeButton of closeButtons) {
+        closeButton.addEventListener("click", (e) => {
+            closeModal(modal, shadow);
+        })
+    }
 }
 
 export function onModalLaunch(e, detail) {
-    const modalType = e.target.dataset.modalId
-    const modal = document.querySelector("#" + modalType)
+    const modalType = e.target.dataset.modalId;
+    const modal = document.querySelector("#" + modalType);
     const modalShownEvent = new CustomEvent("modal.shown", {
         detail
     })
 
-    modal.dispatchEvent(modalShownEvent)
+    modal.dispatchEvent(modalShownEvent);
 
-    const shadow = document.querySelector(".modal-shadow")
-    modal.classList.add("show");
-    shadow.classList.add("show");
+    const shadow = document.querySelector(".modal-shadow");
+    showModal(modal, shadow)
 
     shadow.addEventListener("click", (e) => {
-        modal.classList.remove("show");
-        shadow.classList.remove("show");
+        closeModal(modal, shadow);
     })
+}
+
+function showModal(modal, shadow) {
+    modal.classList.add("show");
+    shadow.classList.add("show");
+}
+
+function closeModal(modal, shadow) {
+    modal.classList.remove("show");
+    shadow.classList.remove("show");
 }
